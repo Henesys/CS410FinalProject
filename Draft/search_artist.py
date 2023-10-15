92bd4c6df44f6da2e191903b1adb8dfe56a79b29
ymd1027 <143768421+ymd1027@users.noreply.github.com>
2023-10-15 17:11:09 -0500

    Update search_artist.py - refines song selection + text data

--- Draft/search_artist.py
+++ Draft/search_artist.py
@@ -1,9 +1,24 @@
-import dash
-from dash import Dash, dcc, html, Input, Output, callback
-from lyricsgenius import Genius
-from PIL import Image
+import re
 import requests
 from io import BytesIO
+
+import dash
+from dash import Dash, dcc, html, Input, Output, callback
+
+from lyricsgenius import Genius
+from PIL import Image
+from textblob import TextBlob
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+
+
+genius = Genius("irpjON-Z1GSt9nz-xhFe-wtKAuFnjrYZ4jP1-9uK6yOQ1dafktyUZzZIQmuGPVkD")
+genius.remove_section_headers = True
+top_song_num = 10
+stop_words = set(stopwords.words('english'))
 
 app = dash.Dash(__name__)
 app.layout = html.Div(
@@ -16,10 +31,35 @@ app.layout = html.Div(
     ]
 )
 
-def process_lyrics(lyrics):
-    lyrics_lines = lyrics.splitlines()
+"""
+Checks if Song Name that is about to be added is already in the Song List.
+"""
+def check_repeat(song_list, new_song_title):
+    song_title = re.sub("\(.*?\)|\[.*?\]","", new_song_title).replace('\u200b', '').strip()
+    if song_title in song_list:
+        return False, song_title
 
-    return lyrics_lines
+    return True, song_title
+
+"""
+Processes Song Lyrics
+-- removes stop words
+-- removes punctuation
+"""
+def process_lyrics(lyrics):
+    lyrics_lines = lyrics.split("Lyrics", 1)[1].strip()
+    if len(lyrics_lines) < 1:
+        lyrics_lines = lyrics
+
+    lyrics_lines = re.sub(r'[^\w\d\s\'\-]+', '', lyrics_lines)
+    lyric_tokens = word_tokenize(lyrics_lines)
+    filtered_lyrics = [w.lower() for w in lyric_tokens if not w.lower() in stop_words]
+
+    blob = TextBlob(' '.join(filtered_lyrics))
+    print(blob.sentiment)
+
+    return filtered_lyrics
+
 
 @callback(
     Output("query_artist_img", "src"),
@@ -27,12 +67,11 @@ def process_lyrics(lyrics):
     Input("submit_artist", "n_clicks"),
 )
 def get_image(query_artist, n_clicks):
-    if n_clicks == 0:
+    if n_clicks == 0 or query_artist == '':
         return None
 
     n_clicks = 0
-    genius = Genius("irpjON-Z1GSt9nz-xhFe-wtKAuFnjrYZ4jP1-9uK6yOQ1dafktyUZzZIQmuGPVkD")
-    artist = genius.search_artist(query_artist, max_songs=1) #max_songs set for testing efficiency
+    artist = genius.search_artist(query_artist, max_songs=1, sort='popularity') #max_songs set for testing efficiency
     artist_img = artist.header_image_url
     response = requests.get(artist_img)
     img = Image.open(BytesIO(response.content))
@@ -50,22 +89,22 @@ def get_lyrics(query_artist, n_clicks):
         return None
 
     n_clicks = 0
-    genius = Genius("irpjON-Z1GSt9nz-xhFe-wtKAuFnjrYZ4jP1-9uK6yOQ1dafktyUZzZIQmuGPVkD")
-    artist = genius.search_artist(query_artist, max_songs=2) #max_songs set for testing efficiency
+    artist = genius.search_artist(query_artist, max_songs=20, sort='popularity') #max_songs set for testing efficiency
     song_list = artist.songs
 
     song_titles = []
     song_lyrics = []
     for song in song_list:
-        song_titles.append(song.title)
+        is_new, song_title = check_repeat(song_titles, song.title)
+        if is_new:
+            song_titles.append(song_title)
 
-        lyrics = process_lyrics(song.lyrics)
-        song_lyrics.append(lyrics)
+            lyrics = process_lyrics(song.lyrics)
+            song_lyrics.append(lyrics)
 
-    songs = dict(zip(song_titles, song_lyrics))
-    print(songs)
+    songs = dict(zip(song_titles[:10], song_lyrics[:10]))
     #artist.save_lyrics()
-    return song.lyrics
+    return ' '.join(song_lyrics[-1])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
