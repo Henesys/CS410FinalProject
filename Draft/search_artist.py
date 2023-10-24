aac926862b3fd8cd1d76248a62af5114e5c78493
ymd1027 <143768421+ymd1027@users.noreply.github.com>
2023-10-23 22:42:50 -0500

    refining text cleaning

--- Draft/search_artist.py
+++ Draft/search_artist.py
@@ -13,9 +13,11 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
+import contractions
+from autocorrect import Speller
 
-
-#genius = Genius("irpjON-Z1GSt9nz-xhFe-wtKAuFnjrYZ4jP1-9uK6yOQ1dafktyUZzZIQmuGPVkD")
+spell = Speller(lang='en')
+#genius = Genius(ACCESS_TOKEN)
 genius.remove_section_headers = True
 top_song_num = 10
 stop_words = set(stopwords.words('english'))
@@ -26,7 +28,6 @@ app.layout = html.Div(
         html.Div(dcc.Input(id='query_artist', type='text', placeholder="Insert Artist Name", debounce=True, minLength=1)),
         html.Button('Submit', id='submit_artist', n_clicks=0),
         html.Br(),
-        html.Img(id="query_artist_img"),
         html.Div(id="query_artist_lyrics"),
     ]
 )
@@ -52,31 +53,33 @@ def process_lyrics(lyrics):
         lyrics_lines = lyrics
 
     lyrics_lines = re.sub(r'[^\w\d\s\'\-]+', '', lyrics_lines)
-    lyric_tokens = word_tokenize(lyrics_lines)
-    filtered_lyrics = [w.lower() for w in lyric_tokens if not w.lower() in stop_words]
+    lyric_tokens = lyrics_lines.split()
 
-    blob = TextBlob(' '.join(filtered_lyrics))
-    print(blob.sentiment)
+    filtered_lyrics = []
+    for w in lyric_tokens:
+        new_w = (contractions.fix(w)).lower()
+
+        # if there are #s in the word, consider it neutral and skip
+        if bool(re.search(r'\d', new_w)):
+            continue
+
+        # if there is an apostrophe in the word
+        if "'" in new_w:
+            new_w = new_w.replace("in'", "ing")
+            new_w = new_w.replace("'bout", "about")
+            new_w = new_w.replace("'til", "until")
+            new_w = new_w.replace("'cause", "because")
+            new_w = new_w.replace("'s", "")
+        else:
+            new_w = spell(new_w)
+
+        # if word is a stop_word, skip
+        if new_w in stop_words:
+            continue
+
+        filtered_lyrics.append(new_w)
 
     return filtered_lyrics
-
-
-@callback(
-    Output("query_artist_img", "src"),
-    Input("query_artist", "value"),
-    Input("submit_artist", "n_clicks"),
-)
-def get_image(query_artist, n_clicks):
-    if n_clicks == 0 or query_artist == '':
-        return None
-
-    n_clicks = 0
-    artist = genius.search_artist(query_artist, max_songs=1, sort='popularity') #max_songs set for testing efficiency
-    artist_img = artist.header_image_url
-    response = requests.get(artist_img)
-    img = Image.open(BytesIO(response.content))
-
-    return img
 
 
 @callback(
@@ -89,7 +92,12 @@ def get_lyrics(query_artist, n_clicks):
         return None
 
     n_clicks = 0
-    artist = genius.search_artist(query_artist, max_songs=20, sort='popularity') #max_songs set for testing efficiency
+
+    try:
+        artist = genius.search_artist(query_artist, max_songs=20, sort='popularity') #max_songs set for testing efficiency
+    except requests.exceptions.Timeout:
+        print ("test")
+
     song_list = artist.songs
 
     song_titles = []
@@ -102,9 +110,19 @@ def get_lyrics(query_artist, n_clicks):
             lyrics = process_lyrics(song.lyrics)
             song_lyrics.append(lyrics)
 
-    songs = dict(zip(song_titles[:10], song_lyrics[:10]))
-    #artist.save_lyrics()
-    return ' '.join(song_lyrics[-1])
+            if len(song_lyrics) >= 10:
+                break
+
+    all_songs = [' '.join(lyric) for lyric in song_lyrics]
+    all_lyrics = ' '.join(all_songs)
+
+    for song in all_songs:
+        blob = TextBlob(song)
+        print(blob.tags)
+        print(blob.noun_phrases)
+        print(blob.sentiment)
+
+    return all_lyrics
 
 if __name__ == '__main__':
     app.run_server(debug=True)
