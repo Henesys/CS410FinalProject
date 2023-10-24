87d16b2a207c49647dadb40e6e5d035c747bae77
chp2 <73094314+chp2@users.noreply.github.com>
2023-10-23 21:09:25 -0500

    Modified title. Made an input box for searching the name

--- spotifydash.py
+++ spotifydash.py
@@ -1,12 +1,29 @@
+import dash
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
+#app = dash.Dash(__name__)
 app = Dash(external_stylesheets=[dbc.themes.UNITED])
+#default_color = default_color = 'rgb(121, 41, 82)'
 
 app.layout = html.Div([
-    html.H1(children = 'SPOTIFY DASHBOARD',style={'textAlign': 'center', 'fontFamily': 'fantasy', 'fontSize': 50, 'color': 'blue'}),
-    html.Div(dcc.Input(type='text')),
-    html.Button('Enter'),
+    dbc.Card(dbc.Row(html.H1(children='Spotify Dashboard', style={'textAlign':'center', 'color': 'green'})), body=True),
+    html.Br(),
+
+    #Widget 1: Searching artist name
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row([
+                html.Div([
+                    html.H4(children= "Enter Artist's name Below"),
+                    dbc.Row([
+                        html.Div(["Name: ", dcc.Input(id='Artist_name', value='Taylor Swift', type='text')])
+                    ], align='right'),
+                    html.Br(),
+                ])
+            ])
+        ])
+    )
 ], style={'padding': 100, 'border': 'solid'})
 
 if __name__ == '__main__':
