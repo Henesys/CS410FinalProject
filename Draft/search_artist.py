4302144c6f0c1c1f014b547134d2215f846f0138
ymd1027 <143768421+ymd1027@users.noreply.github.com>
2023-10-30 22:26:12 -0500

    updated word_cloud generation logic

--- Draft/search_artist.py
+++ Draft/search_artist.py
@@ -12,9 +12,15 @@ from textblob import TextBlob
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
 import contractions
 from autocorrect import Speller
+
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import pandas as pd
 
 spell = Speller(lang='en')
 #genius = Genius(ACCESS_TOKEN)
@@ -28,7 +34,7 @@ app.layout = html.Div(
         html.Div(dcc.Input(id='query_artist', type='text', placeholder="Insert Artist Name", debounce=True, minLength=1)),
         html.Button('Submit', id='submit_artist', n_clicks=0),
         html.Br(),
-        html.Div(id="query_artist_lyrics"),
+        html.Img(id="query_artist_lyrics"),
     ]
 )
 
@@ -81,9 +87,40 @@ def process_lyrics(lyrics):
 
     return filtered_lyrics
 
+"""
+Generates Word Cloud
+"""
+def word_cloud(lyrics):
+    token_lyrics = word_tokenize(lyrics)
+    pos_lyrics = nltk.pos_tag(token_lyrics)
+
+    lyrics_out = ' '.join([word for (word, pos) in pos_lyrics if pos == 'NN'])
+
+    with open('top100common.txt') as f:
+        common_words = f.read().split()
+
+    stopwords = set(STOPWORDS).union(set(common_words))
+
+    wordcloud = WordCloud(width = 800, height = 800,
+                    background_color ='white',
+                    stopwords = stopwords,
+                    collocations=False,
+                    min_font_size = 10).generate(lyrics_out)
+
+    # plot the WordCloud image  
+    plt.switch_backend('Agg')                     
+    plt.figure(figsize = (8, 8), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+
+    plt.savefig('word_cloud.png')
+
+    im  = Image.open('word_cloud.png')
+    return im
 
 @callback(
-    Output("query_artist_lyrics", "children"),
+    Output("query_artist_lyrics", "src"),
     Input("query_artist", "value"),
     Input("submit_artist", "n_clicks"),
 )
@@ -93,10 +130,18 @@ def get_lyrics(query_artist, n_clicks):
 
     n_clicks = 0
 
-    try:
-        artist = genius.search_artist(query_artist, max_songs=20, sort='popularity') #max_songs set for testing efficiency
-    except requests.exceptions.Timeout:
-        print ("test")
+    test_count = 0
+    while True:
+        try:
+            artist = genius.search_artist(query_artist, max_songs=20, sort='popularity') #max_songs set for testing efficiency
+            break
+        except requests.exceptions.Timeout:
+            print ("genius execution failed, trying again")
+            test_count += 1
+
+            if test_count > 5:
+                print ("genius execution failed, please rerun program")
+                sys.out(1)
 
     song_list = artist.songs
 
@@ -116,13 +161,17 @@ def get_lyrics(query_artist, n_clicks):
     all_songs = [' '.join(lyric) for lyric in song_lyrics]
     all_lyrics = ' '.join(all_songs)
 
+    print(all_lyrics)
+
+    im = word_cloud(all_lyrics)
+
     for song in all_songs:
         blob = TextBlob(song)
         print(blob.tags)
         print(blob.noun_phrases)
         print(blob.sentiment)
 
-    return all_lyrics
+    return im
 
 if __name__ == '__main__':
     app.run_server(debug=True)
