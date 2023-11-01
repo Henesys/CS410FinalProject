5abda85e18e8884e5296e576667cc6b28b0b49cb
Henesys <hyosup2@illinois.edu>
2023-11-01 18:35:53 -0500

    Changed to song_data and csv_analysis, t- test requires significant optimizations 110123

--- spotifydash.py
+++ spotifydash.py
@@ -1,5 +1,5 @@
 #connecting genius & spotify
-from draft.search_artist import * #from Marcia
+from draft.search_artist import *
 
 #Dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
@@ -8,6 +8,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import base64
+
 #import os
 from PIL import Image
 
