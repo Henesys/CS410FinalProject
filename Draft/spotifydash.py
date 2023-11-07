f116c5b03090ff14d40093d3a298a0e5d22ef821
chp2 <73094314+chp2@users.noreply.github.com>
2023-11-07 17:29:28 -0600

    wordcloud image added

--- Draft/spotifydash.py
+++ Draft/spotifydash.py
@@ -1,5 +1,5 @@
 #connecting genius & spotify
-from search_artist.py import process_artist_lyrics
+#from search_artist.py import process_artist_lyrics
 
 #Dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
@@ -8,11 +8,9 @@ from dash.dependencies import Input, Output, State
 import base64
 from PIL import Image
 from textblob import TextBlob
-
+from os import path
 import app
 spt_img = Image.open("Draft\\spotify.png")
-image_path = "Draft\\sample_TaylorSwift_WordCloud.png"
-pil_img = Image.open(image_path)
 
 lines_list = open("ArtistLyrics//artists.txt" ).read().splitlines()
 
@@ -28,16 +26,51 @@ app.layout = html.Div([
             dbc.Row([
                 html.Div([
                     #Search Artist's name
-    
+
                     html.H4(children= "Enter Artist's name Below"),
                     dbc.Row([
-                        html.Div([html.Img(src=pil_img), "Name: ", dcc.Input(id='query_artist', value='Taylor Swift', type='text'), html.Button('Submit', id='submit_artist', n_clicks=0)])
+                        html.Div(["Name: ", dcc.Input(id='query_artist', value= "Taylor Swift", type='text'), html.Button('Submit', id='submit_artist', n_clicks=0)])
                     ], align='right'),
+                    html.H4(children ="Artist"),
+                    #html.Img(src = Image.open("Draft//taylorswift_image.png")),
+                    html.Img(id='image',style={'height':'=50%', 'width':'20%'}),
                     html.Br(),
                     html.H4(children ="wordcloud"),
-                    html.Img(src=pil_img),
-                    #html.Img(id='word_cloud', src=pil_img),
+                    html.Img(id='word_cloud',style={'height':'=50%', 'width':'50%'}),
                     html.Br(),
+                    html.H4(children ="Analysis"),
+                    dcc.Textarea(id='analysis', value='artist analysis', style={'width': '100%', 'height': 300})
+                ])
+            ])
+        ])
+    )
+], style={'padding': 100, 'border': 'solid'})
+
+
+
+@callback(
+    Output("image", "src"), #artist image
+    Output("word_cloud", "src"), #word cloud
+    #Output("Analysis", value), 
+    Input("submit_artist", "n_clicks"),
+    State("query_artist", "value")
+)
+def get_results(n_clicks, query_artist):
+    artist = query_artist.lower().replace(" ", "")
+    if n_clicks == 0:
+        return None
+    if artist in lines_list:
+        return Image.open("ArtistLyrics//" + artist + "_image" + "." + "jpg"),  Image.open("ArtistLyrics//" + artist + "_wordCloud" + "." + "png")
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+'''
+                   html.Br(),
+
                     dash_table.DataTable(id='table',
                                 columns = [{'name':'Artist Image','id':'image'}, 
                                         {'name':'analysis','id':'any'}],
@@ -49,45 +82,14 @@ app.layout = html.Div([
                                 style_header={'backgroundColor':default_color,'color':'green'}
                                 #ch
                     )
-                ])
-            ])
-        ])
-    )
-], style={'padding': 100, 'border': 'solid'})
+'''
 
-
-
-@callback(
-    Output("word_cloud", "src"),
-    Input("query_artist", "value"),
-    Input("submit_artist", "n_clicks"),
-)
-def get_word_cloud(query_artist,  n_clicks):
+'''
+def get_results(query_artist,  n_clicks):
     if n_clicks == 0:
         return None
-    if query_artist in lines_list:
-    lyrics = get_lyrics(query_artist)
-    img_wordcloud = word_cloud(lyrics)
-    return img_wordcloud
-
+    artist = query_artist.lower().replace(" ", "")
+    if artist in lines_list:
+        Artist_image = "ArtistLyrics//" + artist + "_image" + "." + "jpg"
+    return Image.open(Artist_image)
 '''
-@callback(
-    Output("table", "data"),
-    Input("query_artist", "value"),
-    Input("submit_artist", "n_clicks"),
-)
-def update_table(input_keyword, n_clicks):
-    if n_clicks == 0:
-        return dash.no_update
-    result = #analysis result
-    return result
-'''
-
-
-        
-    
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
