a1d691181c66ae08da37f379745fa9a833cba035
ymd1027 <143768421+ymd1027@users.noreply.github.com>
2023-12-09 10:25:54 -0600

    testing installation process

--- Final/Scripts/spotify.py
+++ Final/Scripts/spotify.py
@@ -134,7 +134,8 @@ def get_artist_face(artist_name):
             # https://developer.spotify.com/documentation/web-api/reference/get-an-artist
             if response.status_code == 200:
                 image = Image.open(BytesIO(response.content))
-                image.save(f"Final/Artists/Images/{artist_name}_image.jpg")
+                image_path = os.path.join(dir, './../Artists/Images/' + artist_name + '_image.jpg')
+                image.save(image_path)
 
                 print(f'Artist\'s Name: {artist["name"]}')
                 print(f"Artist's face saved as {artist_name}_face.jpg")
